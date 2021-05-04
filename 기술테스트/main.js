c36d8523602cc025d206f77a5411612604956ef0
윤성준 <75359085+ytw418@users.noreply.github.com>
2021-05-04 17:39:56 +0900

    애니메이션 추가

--- 기술테스트/main.js
+++ 기술테스트/main.js
@@ -47,23 +47,6 @@ function dynamic(randomArr)
 
 dynamic(randomString());
 
-class sec1_main1{
-    static attachEvent = ()=>{
-        window.addEventListener('scroll',()=>{
-            for(let i=0; i<Portpolio.getElementPortpolioItem().length;i++){
-                setTimeout(()=>{
-                    if(window.scrollY > 2700){
-                        Portpolio.getElementPortpolioItem()[i].classList.add('visible');
-                    }
-                },i*300);
-            }
-        });
-    }
-
-    static getElementPortpolio = ()=>{
-        return window.document.body.querySelector('[rel="js-portpolio"]');
-    }
-    static getElementPortpolioItem = ()=>{
-        return Portpolio.getElementPortpolio().querySelectorAll('.grid-item');
-    }
-}
+$(document).ready(function(){
+    $('.bxslider').bxSlider();
+  });
