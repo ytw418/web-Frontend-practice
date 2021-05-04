d0c68df88fd9566322e4ffd989d170c1f7aaa481
윤성준 <75359085+ytw418@users.noreply.github.com>
2021-05-04 09:10:37 +0900

    애니메이션 기능 오류 수정

--- 기술테스트/main.js
+++ 기술테스트/main.js
@@ -1,43 +1,69 @@
-$(function(){
-    setTimeout(function(){
-        var sc_height = $(document).scrollTop();
-        var quick = $('#quick');
-        var break_point1 = $('#sec1').offset().top;
-        var break_point2 = $('#footer').offset().top;
-        var footer_height = $('#footer').height();
-        var se2_height = $('#sec1').height();
-        //console.log(break_point1);
-        //console.log(break_point2);
-        //console.log(footer_height);
-        //console.log(se2_height);
-        console.log(break_point2-footer_height);
-        console.log(se2_height-break_point1);
-        if(sc_height<break_point1){
-            $(quick).hide();
-        }
-        $(window).scroll(function(){
-            var sc_height = $(document).scrollTop();
-            
-            //console.log(sc_height)
-            if(sc_height>se2_height-break_point1){
-                $(quick).stop();
-                $(quick).show();
-            }
-            if(sc_height<se2_height-break_point1){
-                $(quick).stop();
-                $(quick).hide();
-            }
-            if(sc_height>break_point2-footer_height){
-                $(quick).stop();
-                $(quick).hide();
+let target = document.querySelector("#dynamic");
+function blink(){
+    target.classList.toggle("active");
+   
+}
+setInterval(blink,500);//커서 깜박임 효과
+
+
+
+
+function randomString()
+{
+let stringArr = ["정확한 분석과 맞춤 솔루션으로"];
+let selectString = stringArr[Math.floor(Math.random() *stringArr.length)];
+let selectStringArr = selectString.split("");
+
+return selectStringArr;
+}
+
+//타이핑 리셋
+function resetTyping()
+{
+    target.textContent = "";
+    dynamic(randomString());
+}
+
+
+
+//한글자씩 텍스트 출력함수
+function dynamic(randomArr)
+{
+  
+    if(randomArr.length > 0)
+    {
+        target.textContent +=randomArr.shift();
+        setTimeout(function()
+        {
+            dynamic(randomArr);
+        },80);
+
+    }
+    else
+    {
+        setTimeout(resetTyping,3000);
+    }
+}
+
+dynamic(randomString());
+
+class sec1_main1{
+    static attachEvent = ()=>{
+        window.addEventListener('scroll',()=>{
+            for(let i=0; i<Portpolio.getElementPortpolioItem().length;i++){
+                setTimeout(()=>{
+                    if(window.scrollY > 2700){
+                        Portpolio.getElementPortpolioItem()[i].classList.add('visible');
+                    }
+                },i*300);
             }
         });
-        $(window).resize(function(){
-            sc_height = $(document).scrollTop();
-            break_point1 = $('#sec1').offset().top;
-            break_point2 = $('#footer').offset().top;
-            footer_height = $('#footer').height();
-            se2_height = $('#sec1').height();
-        })
-    },500);
-});
+    }
+
+    static getElementPortpolio = ()=>{
+        return window.document.body.querySelector('[rel="js-portpolio"]');
+    }
+    static getElementPortpolioItem = ()=>{
+        return Portpolio.getElementPortpolio().querySelectorAll('.grid-item');
+    }
+}
