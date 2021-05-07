7c2895360379f120703de0a58565954eb8df9fe5
윤성준 <75359085+ytw418@users.noreply.github.com>
2021-05-07 18:27:44 +0900

    미디어 쿼리 반응형 기능 추가 3번섹션 애니메이션 기능 추가

--- 기술테스트/main.js
+++ 기술테스트/main.js
@@ -10,7 +10,7 @@ setInterval(blink,500);//커서 깜박임 효과
 
 function randomString()
 {
-let stringArr = ["정확한 분석과 맞춤 솔루션으로"];
+let stringArr = ["가장 효율적인 결과물을 만나보세요"];
 let selectString = stringArr[Math.floor(Math.random() *stringArr.length)];
 let selectStringArr = selectString.split("");
 
@@ -74,3 +74,21 @@ class Arrow{
         }
     }
 }
+// 네비게이션 바 
+function navigo (){
+    const header = document.querySelector('header'); //헤더부분획득
+    const headerheight = header.clientHeight;//헤더높이
+  document.addEventListener('scroll', onScroll, { passive: true });//스크롤 이벤트
+   function onScroll () {
+       const scrollposition = pageYOffset;//스크롤 위치
+     const nav = document.querySelector('header');//네비게이션
+     if (headerheight<=scrollposition){//만약 헤더높이<=스크롤위치라면
+       nav.classList.add('fix')//fix클래스를 네비에 추가
+     }
+     else {//그 외의 경우
+       nav.classList.remove('fix');//fix클래스를 네비에서 제거
+     }
+   } 
+    
+  }
+  navigo()
