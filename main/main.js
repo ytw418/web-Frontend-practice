05a11fa4e8ea6703a06006fdb75e10c43277ee9f
윤성준 <75359085+ytw418@users.noreply.github.com>
2021-06-04 09:17:02 +0900

    어제 수정한거 뭔지 까먹음

--- main/main.js
+++ main/main.js
@@ -51,32 +51,7 @@ dynamic(randomString());
 
 
 
-class Arrow{
-    static attachEvent = ()=>{
-        window.addEventListener('scroll',()=>{
-            if(window.scrollY > 300){
-                Arrow.move();
-            }else{
-                Arrow.unMove();
-            }
-        });
-    }
 
-    static getElementArrow = ()=>{
-        return window.document.body.querySelector('[rel="js-scroll_Arrow"]');
-    }
-
-    static move = ()=>{
-        if(!Arrow.getElementArrow().classList.contains('move')){
-            Arrow.getElementArrow().classList.add('move');
-        }
-    }
-    static unMove = ()=>{
-        if(Arrow.getElementArrow().classList.contains('move')){
-            Arrow.getElementArrow().classList.remove('move');
-        }
-    }
-}
 // 네비게이션 바 
 function navigo (){
     const header = document.querySelector('header'); //헤더부분획득
